dd5397fff65b047192021991b1883c296680a65f
Steven Buxton <Breathesteve@gmail.com>
2015-07-17 15:35:14 +0000

    Created contact form view

--- db/schema.rb
+++ db/schema.rb
@@ -14,6 +14,11 @@
 ActiveRecord::Schema.define(version: 20150713135344) do
 
   create_table "contacts", force: :cascade do |t|
+    t.string   "name"
+    t.string   "email"
+    t.text     "comments"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
 end
