29e8be1f6a69efdef6ff0a4080333561a8a8f251
Steven Buxton <Breathesteve@gmail.com>
2015-10-23 04:21:06 +0000

    Added association for plan and users

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151023035853) do
+ActiveRecord::Schema.define(version: 20151023041715) do
 
   create_table "contacts", force: true do |t|
     t.string   "name",       limit: nil
@@ -41,6 +41,7 @@ ActiveRecord::Schema.define(version: 20151023035853) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.integer  "plan_id"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
