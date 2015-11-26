c9609db218eb9c1ce9ca63d0a6b093a768143d84
Steven Buxton <Breathesteve@gmail.com>
2015-11-26 08:10:09 +0000

    Updated routes file with new devise controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,9 +6,9 @@ class ApplicationController < ActionController::Base
   before_filter :configure_permitted_parameters, if: :devise_controller?
   
   protected
-  def configure_permitted_parameters
+    def configure_permitted_parameters
     
     devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:name, :stripe_card_token, :email, :password, :password_confirmation) }
     
-  end
+    end
 end
