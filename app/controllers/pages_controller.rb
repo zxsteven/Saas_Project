3e854302da32ce555778b051aa4b46c1124a0c97
Steven Buxton <Breathesteve@gmail.com>
2015-11-30 08:45:30 +0000

    fixed heroku

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,9 +1,9 @@
 class PagesController < ApplicationController
-    def home
-        @basic_plan = Plan.find(1)
-        @pro_plan = Plan.find(2)
-    end
-    
-    def about
-    end
+  def home
+    @basic_plan = Plan.find(1)
+    @pro_plan = Plan.find(2)
+  end
+  
+  def about
+  end
 end
