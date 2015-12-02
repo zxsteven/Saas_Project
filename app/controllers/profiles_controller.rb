18f6306c1f19d82e12d639f719bea6b976f45328
Steven Buxton <Breathesteve@gmail.com>
2015-12-02 21:17:11 +0000

    Added before filters to check user

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,10 +1,13 @@
 class ProfilesController < ApplicationController
-    def new
-       # form where a user can fill out their own profile.
-        @user = User.find( params[:user_id] )
-        @profile = Profile.new
-    end
-    
+  before_action :authenticate_user!
+  before_action :only_current_user
+  
+  def new
+    # form where a user can fill out their own profile.
+    @user = User.find( params[:user_id] )
+    @profile = Profile.new
+  end
+  
   def create 
     @user = User.find( params[:user_id] )
     @profile = @user.build_profile(profile_params)
@@ -15,13 +18,30 @@ class ProfilesController < ApplicationController
       render action: :new
     end
   end
-    def edit
-      @user = User.find( params[:user_id] )
-      @profile = @user.profile
+  
+  def edit
+    @user = User.find( params[:user_id] )
+    @profile = @user.profile
+  end
+  
+  def update
+    @user = User.find( params[:user_id] )
+    @profile = @user.profile
+    if @profile.update_attributes(profile_params)
+      flash[:success] = "Profile Updated!"
+      redirect_to user_path( params[:user_id] )
+    else
+      render action: :edit
     end
-    
-    private
-        def profile_params
-            params.require(:profile).permit(:first_name, :last_name, :job_title, :phone_number, :contact_email, :description)
-        end
+  end
+  
+  private
+    def profile_params
+      params.require(:profile).permit(:first_name, :last_name, :avatar, :job_title, :phone_number, :contact_email, :description)
+    end
+  
+    def only_current_user
+      @user = User.find( params[:user_id] )
+      redirect_to(root_url) unless @user == current_user
+    end
 end
