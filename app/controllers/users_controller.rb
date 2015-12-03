f214914b1fef5f1061c6e7c31aef1d8efad191e7
Steven Buxton <Breathesteve@gmail.com>
2015-12-03 15:30:26 +0000

    improved users index page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,9 @@
 class UsersController < ApplicationController
       before_action :authenticate_user!
       
-      def index
-          
-      end
+    def index
+      @users = User.all  
+    end
       
     def show
        @user = User.find( params[:id] ) 
